e0b7f06a4fd7b32cf432b3e2ee6aa44bafa11990
AirtonViniciusFerreira <49006845+AirtonViniciusFerreira@users.noreply.github.com>
2020-07-10 15:24:19 -0300

    correções
    correções

---
+++ js/tema.js
@@ -0,0 +1,9 @@
+function temaBlack(){
+    document.getElementById("tema").href = "/css/styleIndexBlack.css";
+    document.getElementById("tema1").href = "/css/styleNavBlack.css";          
+}
+
+function temaNorm(){
+    document.getElementById("tema").href = "/css/styleIndex.css";
+    document.getElementById("tema1").href = "/modulo1/aula2/css/styleNav.css";
+}
