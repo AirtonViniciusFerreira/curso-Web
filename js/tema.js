7a596196e0115ea2a7239074bfdd52e9bc1902de
AirtonViniciusFerreira <49006845+AirtonViniciusFerreira@users.noreply.github.com>
2020-07-10 16:09:04 -0300

    removendo css sem pastas

--- js/tema.js
+++ js/tema.js
@@ -1,9 +1,9 @@
 function temaBlack(){
-    document.getElementById("tema").href = "styleIndexBlack.css";
-    document.getElementById("tema1").href = "styleNavBlack.css";          
+    document.getElementById("tema").href = "css/styleIndexBlack.css";
+    document.getElementById("tema1").href = "css/styleNavBlack.css";          
 }
 
 function temaNorm(){
-    document.getElementById("tema").href = "styleIndex.css";
+    document.getElementById("tema").href = "css/styleIndex.css";
     document.getElementById("tema1").href = "modulo1/aula2/css/styleNav.css";
 }
